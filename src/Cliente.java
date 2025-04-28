f3ad160eb3ef443ad214c0647420b39fd606cff0
FabrizzioFederico <Fabrizziofederico@hotmail.com>
2025-04-27 21:44:03 -0300

    Se finalizo el prototipo de Herencia_Usuario

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 
 public class Cliente extends Usuario{
 	
@@ -16,6 +17,32 @@ public class Cliente extends Usuario{
 		this.direccion = direccion;
 	}
 	
+	public void realizarCompra() {
+		String menu [] = {"Seleccionar Producto","Pagar","Salir"};
+		int elegido = 0;
+		do {
+			elegido = JOptionPane.showOptionDialog(null, "Seleccione una Opción", "Jumbox", 0, 0, null, menu, menu[0]);
+			switch (elegido) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "En Proceso...");
+				break;
+			case 1:
+				JOptionPane.showMessageDialog(null, "En Proceso...");
+				break;
+			}
+		} while (elegido!=2);
+	}
 	
-	
-}
+	public void verCompra() {
+		String menu [] = {"Ver Compra","Salir"};
+		int elegido = 0;
+		do {
+			elegido = JOptionPane.showOptionDialog(null, "Seleccione una Opción", "Jumbox", 0, 0, null, menu, menu[0]);
+			switch (elegido) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "En Proceso...");
+				break;
+			}
+		} while (elegido!=1);
+		}
+	}
