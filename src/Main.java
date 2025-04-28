073c65ad54dc42019d4bb0baa3e0f49935489b6c
FabrizzioFederico <Fabrizziofederico@hotmail.com>
2025-04-27 22:12:42 -0300

    Se crearon objetos.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Cliente c1 = new Cliente(1,"firmin","firmin.lopez@davinci.edu.ar","dasdasd","Jumbox","Av Corrientes eu eu");
-		//c1.realizarCompra();
-		//c1.verCompra();
+		Cliente c1 = new Cliente(1,"firmin","firmin.lopez@davinci.edu.ar","dasdasd","Jumbox","Av Corrientes eu eu");
+		c1.realizarCompra();
+		c1.verCompra();
 		//Encargado_Stock e1 = new Encargado_Stock(1,"mantequilla","mantequilla.lopez@davinci.edu.ar","dasdasd","Jumbox");
 		//e1.actualizarStock();
 		//e1.verStock();
